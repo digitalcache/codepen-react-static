c44b8e24f01e1768195e79efbbf01d63b24868ea
dcharlis <dcharlis@pkglobal.com>
2021-07-19 22:22:03 +0530

    Adding files

---
+++ precache-manifest.a062a5da6aeb74a3bc677c213b9b1c21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dec1c3aa66b68d519b6f999ab5b2ec76",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2438ad1cd03ec7bdf574",
+    "url": "./static/css/2.ce280647.chunk.css"
+  },
+  {
+    "revision": "4c124dea4a8534740bd6",
+    "url": "./static/css/main.cba244e8.chunk.css"
+  },
+  {
+    "revision": "2438ad1cd03ec7bdf574",
+    "url": "./static/js/2.3c33bb85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.3c33bb85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c124dea4a8534740bd6",
+    "url": "./static/js/main.69e9c4e2.chunk.js"
+  },
+  {
+    "revision": "dea1ef8f2a1d25e01e7c",
+    "url": "./static/js/runtime-main.2a7eb8aa.js"
+  }
+]);
